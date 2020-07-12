d162dd5580f92c43247a313ff3af82b39b65cf81
Mikhail Komarov <nemo@nil.foundation>
2020-07-12 20:54:22 +0300

    Word bits macro replaced #1

--- include/boost/crypto3/detail/reverser.hpp
+++ include/boost/crypto3/detail/reverser.hpp
@@ -44,12 +44,12 @@ namespace boost {
              */
             inline void reverse_byte(byte_type &b) {
 
-#if (CRYPTO3_MP_WORD_BITS == 32)
+#if (BOOST_ARCH_CURRENT_WORD_BITS== 32)
                 b = unbounded_shr<16>(((b * 0x0802LU & 0x22110LU) | (b * 0x8020LU & 0x88440LU)) * 0x10101LU);
-#elif (CRYPTO3_MP_WORD_BITS == 64)
+#elif (BOOST_ARCH_CURRENT_WORD_BITS== 64)
                 b = (b * 0x0202020202ULL & 0x010884422010ULL) % 1023;
 #else
-#error "CRYPTO3_MP_WORD_BITS not set"
+#error "BOOST_ARCH_CURRENT_WORD_BITSnot set"
 #endif
             }
 
